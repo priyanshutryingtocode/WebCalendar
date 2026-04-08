64f20672f69afce1dd9fa5e903eb88615acd64ca
Priyanshu Srivastava <priyanshusrivastava406@gmail.com>
2026-04-09 02:26:26 +0530

    final deployment

--- webcalendar/src/App.jsx
+++ webcalendar/src/App.jsx
@@ -1,9 +1,7 @@
-// src/App.jsx
 import Calendar from './components/Calendar';
 
 function App() {
   return (
-    // Added dark transition and dark background color
     <div className="w-full min-h-screen bg-slate-100 dark:bg-slate-900 transition-colors duration-300 flex items-center justify-center">
       <Calendar />
     </div>
